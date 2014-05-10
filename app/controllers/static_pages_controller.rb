be7103ecddc0f24b48f7bf30600a92396dc111c5
jongilman <jon.gilman@gmail.com>
2014-05-10 10:53:13 -0400

    Added static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def terms
+  end
+
+  def privacypolicy
+  end
+
+  def support
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/terms'
+
+  get 'static_pages/privacypolicy'
+
+  get 'static_pages/support'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
